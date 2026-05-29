d6711e2ed45ddff7fa1081b117db4a5343c070ee
GibranMaulana <narbiganaluam@gmail.com>
2026-05-29 13:29:43 +0700

    build: configure Hilt DI, ZXing, and Compose navigation dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,15 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.59.2")
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
 }
